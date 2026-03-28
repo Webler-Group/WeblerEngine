1226cebd2903f1c1b85f247a98fd2733cb22f407
DonDejvo <dada542cz@gmail.com>
2026-03-28 13:36:42 +0100

    body to scene node

--- src/physics.js
+++ src/physics.js
@@ -1,12 +1,17 @@
-class Body {
-    static bodyCount = 0;
-    constructor(x, y, mass = 1, inertia = 1000) {
-        this.id = Body.bodyCount++;
-        this.name = "Body-" + this.id;
-        this.pos = new Vector(x, y);
+/**
+ * @typedef {Object} BodyParams
+ * @property {number} [mass=1]
+ * @property {number} [inertia=1000]
+ */
+
+class Body extends SceneNode {
+    /** @param {BodyParams} [params] */
+    constructor({ mass = 1, inertia = 1000 } = {}) {
+        super();
+        this.physPos = new Vector(0, 0);
         this.vel = new Vector(0, 0);
         this.acc = new Vector(0, 0);
-        this.angle = 0;
+        this.physAngle = 0;
         this.angVel = 0;
         this.angAcc = 0;
         this.mass = mass;
@@ -26,18 +31,18 @@ class Body {
     applyForce(force) { this.force.add(force); }
     applyForceAtPoint(force, point) {
         this.force.add(force);
-        const r = Vector.sub(point, this.pos);
+        const r = point.clone().sub(this.physPos);
         this.torque += r.x * force.y - r.y * force.x;
     }
     integrateVelocity(dt) {
         if (this.invMass === 0) return;
-        this.vel.add(new Vector(this.force.x * this.invMass * dt, this.force.y * this.invMass * dt));
+        this.vel.add(this.force.clone().scale(this.invMass * dt));
         this.angVel += this.torque * this.invInertia * dt;
     }
     integratePosition(dt) {
         if (this.invMass === 0) return;
-        this.pos.add(Vector.mult(this.vel, dt));
-        this.angle += this.angVel * dt;
+        this.physPos.add(this.vel.clone().scale(dt));
+        this.physAngle += this.angVel * dt;
     }
     clearForces() { this.force.set(0, 0); this.torque = 0; }
     update(dt) {
